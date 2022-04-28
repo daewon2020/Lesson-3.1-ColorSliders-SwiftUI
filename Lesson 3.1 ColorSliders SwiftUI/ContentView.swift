e211113659b8325383b162a65995b7aec50e513e
Kostya Andreev <daewon@yandex.ru>
2022-04-28 16:05:41 +0300

    Initial Commit

---
+++ Lesson 3.1 ColorSliders SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lesson 3.1 ColorSliders SwiftUI
+//
+//  Created by Константин Андреев on 28.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson 3.1 ColorSliders SwiftUI/Lesson_3_1_ColorSliders_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_3_1_ColorSliders_SwiftUIApp.swift
+//  Lesson 3.1 ColorSliders SwiftUI
+//
+//  Created by Константин Андреев on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_3_1_ColorSliders_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
